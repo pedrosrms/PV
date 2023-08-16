f3b2861229d89de0ab03d9d5ba5a14ab144d516d
pedrosrms <128639292+pedrosrms@users.noreply.github.com>
2023-08-16 14:02:27 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER3
+ */
+public class Main {
+    public static void main(String [] args){
+        Menu menu = new Menu();
+        menu.setVisible(false);
+        
+        CadastroVendas cadVend = new CadastroVendas
+    }
+}
